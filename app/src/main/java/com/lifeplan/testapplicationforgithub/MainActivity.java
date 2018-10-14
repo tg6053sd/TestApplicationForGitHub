613b175937673be572ed24d709a57ab2c0512365
tg6053sd <44040810+tg6053sd@users.noreply.github.com>
2018-10-14 10:03:21 +0900

    add change text button

--- app/src/main/java/com/lifeplan/testapplicationforgithub/MainActivity.java
+++ app/src/main/java/com/lifeplan/testapplicationforgithub/MainActivity.java
@@ -2,12 +2,27 @@ package com.lifeplan.testapplicationforgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(String.valueOf(count) + "回目");
+                count += 1;
+            }
+        });
     }
 }
